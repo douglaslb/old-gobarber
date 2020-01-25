ba06e1f37098af0267552792d37558af8092d0ab
Douglas <doug.lb07@gmail.com>
2020-01-25 14:18:55 -0300

    Creating model and migration to upload files

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize'
 import databaseConfig from '../config/database'
 
 import users from '../app/models/User'
+import file from '../app/models/File'
 
-const models = [users]
+const models = [users, file]
 
 class Database {
   constructor() {
@@ -13,7 +14,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig)
 
-    models.map(model => model.init(this.connection))
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
   }
 }
 
