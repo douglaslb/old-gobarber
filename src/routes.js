480431004a26faa8678796184197717f1be9f3d0
Douglas <doug.lb07@gmail.com>
2020-01-27 13:39:21 -0300

    Listing all providers and their avatar

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
+import ProviderController from './app/controllers/ProviderController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -20,5 +21,7 @@ routes.use(authMiddleware)
 routes.put('/users', UserController.update)
 
 routes.post('/files', upload.single('file'), FileController.store)
+
+routes.get('/providers', ProviderController.index)
 
 export default routes
