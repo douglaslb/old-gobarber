6083bf827ecd9fb7de7f32c1ca622724f33dacda
Douglas <doug.lb07@gmail.com>
2020-01-27 19:32:00 -0300

    Validating available dates with date-fns

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup'
+import { startOfHour, parseISO, isBefore } from 'date-fns'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
 
@@ -26,6 +27,34 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointment with providers' })
+    }
+
+    /*
+     * Checking past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date))
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' })
+    }
+
+    /*
+     * Checking date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceledAt: null,
+        date: hourStart,
+      },
+    })
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' })
     }
 
     const appointment = await Appointment.create({
