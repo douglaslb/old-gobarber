f4c7617ab3b54de40faa9620c292c8f055e099d0
Douglas <doug.lb07@gmail.com>
2020-01-28 11:22:09 -0300

    Adding nodemailer to send emails using mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '43d6bf68c08a50',
+    pass: '997befe2d55ca2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
