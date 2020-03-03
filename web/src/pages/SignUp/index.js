b2a186afc41777ecf705f157fce32dbd1ba832c0
Douglas Lima <doug.lb07@gmail.com>
2020-03-02 23:51:14 -0300

    feat: implementing sign up request

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Form, Input } from '@rocketseat/unform'
 import * as Yup from 'yup'
+
+import { signUpRequest } from '~/store/modules/auth/actions'
 
 import logo from '~/assets/logo.svg'
 
@@ -16,8 +19,11 @@ const schema = Yup.object().shape({
 })
 
 export default function SignUp() {
-  const handleSubmit = data => {
-    console.tron.log(data)
+  const dispatch = useDispatch()
+  const loading = useSelector(state => state.auth.loading)
+
+  const handleSubmit = ({ name, email, password }) => {
+    dispatch(signUpRequest(name, email, password))
   }
 
   return (
@@ -32,7 +38,9 @@ export default function SignUp() {
           placeholder="Sua senha secreta"
         />
 
-        <button type="submit">Criar conta</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Criar conta'}
+        </button>
         <Link to="/">Já tenho login</Link>
       </Form>
     </>
--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -3,7 +3,7 @@ import history from '~/services/history'
 import { toast } from 'react-toastify'
 import api from '~/services/api'
 
-import { signInSuccess, signFailure } from './actions'
+import { signInSuccess, signFailure, signUpSuccess } from './actions'
 
 export function* signIn({ payload }) {
   try {
@@ -31,4 +31,26 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)])
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    })
+
+    yield put(signUpSuccess())
+    history.push('/')
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados')
+    yield put(signFailure())
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+])
