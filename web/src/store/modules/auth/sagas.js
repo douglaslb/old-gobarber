935e0bd24834f8e2552f190a86a283cad92e141f
Douglas Lima <doug.lb07@gmail.com>
2020-03-03 00:19:58 -0300

    feat: setting token authentication into headers request

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -21,6 +21,7 @@ export function* signIn({ payload }) {
       yield put(signFailure())
       return
     }
+    api.defaults.headers.Authorization = `Bearer ${token}`
 
     yield put(signInSuccess(token, user))
 
@@ -50,7 +51,20 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) {
+    return
+  }
+
+  const { token } = payload.auth
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ])
