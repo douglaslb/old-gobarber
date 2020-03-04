ab71d7c9ff19fccccfd98152a348fe2ea997af9f
Douglas Lima <doug.lb07@gmail.com>
2020-03-04 18:23:17 -0300

    feat: implementing user profile image update

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -27,7 +27,7 @@ export default function Header() {
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
               alt="Douglas Lima"
             />
           </Profile>
