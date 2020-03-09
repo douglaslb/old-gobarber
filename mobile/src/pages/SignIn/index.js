9a3b78f52f8fe6ec58b6f4c1d1dd6daefe2c59fe
Douglas Lima <doug.lb07@gmail.com>
2020-03-09 13:53:12 -0300

    feat: creating input and button component

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Text} from 'react-native'
 
+import Input from '~/components/Input'
 import Background from '~/components/Background'
+import Button from '~/components/Button'
 
 export default function SignIn() {
   return (
     <Background>
       <Text>Hello</Text>
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
     </Background>
   )
 }
