108958bc8ae1c32059ed87835af6021ed543f085
Douglas Lima <doug.lb07@gmail.com>
2020-03-09 12:23:57 -0300

    configuring authentication routes

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {View} from 'react-native'
+
+import Routes from './routes'
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />
+  return <Routes />
 }
